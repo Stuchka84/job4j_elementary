733d9cd0f5e82f571cfcf9abea442c940ea7af9d
Stuchka84 <tanya.mjakshina@gmail.com>
2021-08-11 08:23:28 +0300

    4.1. Аргументы [#295521]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -9,6 +9,7 @@ public class ArgMethod {
     public static void main(String[] args) {
         String name = "Job4j";
         int age = 33;
+
         ArgMethod.hello(name);
         ArgMethod.hello(name);
         ArgMethod.hello(name);
